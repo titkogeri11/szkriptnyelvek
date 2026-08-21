87ee620209a8346d46a6e6e36b90e40354717d90
titkogergo <titkogergo@protonmail.com>
2026-08-21 18:49:26 +0200

    mentve

--- expense.py
+++ expense.py
@@ -1,3 +1,28 @@
+import os
+def clear_screen():
+    os.system("clear" if os.name != "nt" else "cls")
+def prompt(message):
+    value = input(message)
+    clear_screen()
+    return value
+def sumexp():
+    with open("expenses.csv", "r") as f:
+        return f.readline()
 
-print("--Kiadáskezelő--")
+def menu():
 
+    #print("1. Összes kiadás")
+    print("2. Új kiadás")
+    print("3. Kiadás törlése")
+    #print("4. ")
+
+def main():
+    menu()
+    mode = prompt("Mód: ")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
